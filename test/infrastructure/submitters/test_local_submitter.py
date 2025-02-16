6a88c467eee6612f0e1a76b3bbbfc467e777696c
igreen1 <iangreen2.00@gmail.com>
2025-02-16 09:28:05 -0500

    Fix pipeline

--- test/infrastructure/submitters/test_local_submitter.py
+++ test/infrastructure/submitters/test_local_submitter.py
@@ -6,14 +6,13 @@ import antz.run
 
 
 def test_local_submitter(tmpdir) -> None:
-    tmpdir = "/home/dominus/tmp"
     dst_file = os.path.join(tmpdir, "end.txt")
 
     src_file = os.path.join(tmpdir, "start.txt")
 
     test_text: str = "Hello there general kenobi"
 
-    with open(src_file, "w") as fh:
+    with open(src_file, "w", encoding='utf-8') as fh:
         fh.write(test_text)
 
     test_config = {
@@ -39,6 +38,6 @@ def test_local_submitter(tmpdir) -> None:
     antz.run.run(test_config)
 
     assert os.path.exists(dst_file)
-    with open(dst_file, "r") as fh:
+    with open(dst_file, "r", encoding='utf-8') as fh:
         ret = fh.read()
     assert ret == test_text
